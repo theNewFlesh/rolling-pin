955646ae53db25fa62d03eb7ed4d416d0385481e
Alex Braun <alexander.g.braun@gmail.com>
2019-12-27 14:05:08 -0800

    is_xxx tests

--- python/rolling_pin/tools_test.py
+++ python/rolling_pin/tools_test.py
@@ -1,4 +1,6 @@
+import json
 import unittest
+from collections import OrderedDict
 
 import rolling_pin.tools as tools
 # ------------------------------------------------------------------------------
@@ -20,3 +22,51 @@ class ToolsTests(unittest.TestCase):
         result = tools.get_ordered_unique(x)
         expected = [0, 1, 2, 3, 4, 5]
         self.assertEqual(result, expected)
+
+    def test_is_iterable(self):
+        self.assertTrue(tools.is_iterable({}))
+        self.assertTrue(tools.is_iterable(OrderedDict({})))
+        self.assertTrue(tools.is_iterable([]))
+        self.assertTrue(tools.is_iterable(set()))
+        self.assertTrue(tools.is_iterable(tuple()))
+
+        self.assertTrue(tools.is_iterable([
+            ('k0', 'v0'),
+            ('k1', 'v1'),
+        ]))
+        self.assertFalse(tools.is_iterable(
+            json.dumps({'a': 'b'})
+        ))
+        self.assertFalse(tools.is_iterable('foo'))
+
+    def test_is_dictlike(self):
+        self.assertTrue(tools.is_dictlike({}))
+        self.assertTrue(tools.is_dictlike(OrderedDict({})))
+
+        self.assertFalse(tools.is_dictlike(
+            json.dumps({'a': 'b'})
+        ))
+        self.assertFalse(tools.is_dictlike([
+            ('k0', 'v0'),
+            ('k1', 'v1'),
+        ]))
+        self.assertFalse(tools.is_dictlike([]))
+        self.assertFalse(tools.is_dictlike(set()))
+        self.assertFalse(tools.is_dictlike(tuple()))
+        self.assertFalse(tools.is_dictlike('foo'))
+
+    def test_is_listlike(self):
+        self.assertTrue(tools.is_listlike([]))
+        self.assertTrue(tools.is_listlike(set()))
+        self.assertTrue(tools.is_listlike(tuple()))
+        self.assertTrue(tools.is_listlike([
+            ('k0', 'v0'),
+            ('k1', 'v1'),
+        ]))
+
+        self.assertFalse(tools.is_listlike({}))
+        self.assertFalse(tools.is_listlike(OrderedDict({})))
+        self.assertFalse(tools.is_listlike(
+            json.dumps({'a': 'b'})
+        ))
+        self.assertFalse(tools.is_listlike('foo'))
