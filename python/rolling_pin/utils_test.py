a169b3e8fca0aaf97c95f5e2d97049cce91b46e0
Alex Braun <alexander.g.braun@gmail.com>
2019-12-27 18:29:14 -0800

    added is_standard_module

--- python/rolling_pin/utils_test.py
+++ python/rolling_pin/utils_test.py
@@ -9,3 +9,9 @@ class UtilsTests(unittest.TestCase):
     def test_relative_path(self):
         result = utils.relative_path(__file__, '../../resources/foo.txt')
         self.assertTrue(os.path.exists(result))
+
+    def test_is_standard_module(self):
+        self.assertTrue(utils.is_standard_module('re'))
+        self.assertTrue(utils.is_standard_module('math'))
+        self.assertFalse(utils.is_standard_module('cv2'))
+        self.assertFalse(utils.is_standard_module('pandas'))
