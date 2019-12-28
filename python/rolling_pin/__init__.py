fc4362065b5b3376f5f2a0f0b2cb85965f9d56c2
Alex Braun <alexander.g.braun@gmail.com>
2019-12-28 11:12:41 -0800

    added pypi files

---
+++ python/rolling_pin/__init__.py
@@ -0,0 +1,12 @@
+'''
+A library of generic tools for ETL work and visualization of JSON blobs and
+python repositories.
+
+Author: Alex Braun
+Email: Alexander.G.Braun@gmail.com
+Github: https://github.com/theNewFlesh
+'''
+from rolling_pin.blob_etl import BlobETL  # noqa F401
+from rolling_pin.repo_etl import RepoETL  # noqa F401
+from rolling_pin import tools             # noqa F401
+from rolling_pin import utils             # noqa F401
---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+    name='rolling-pin',
+    packages=['rolling-pin'],
+    version='0.1.0',
+    license='MIT',
+    description='A library of generic tools for ETL work and visualization of JSON blobs and python repositories.',  # noqa E501
+    author='Alex Braun',
+    author_email='Alexander.G.Braun@gmail.com',
+    url='https://github.com/theNewFlesh/rolling-pin',
+    download_url='https://github.com/theNewFlesh/rolling-pin/archive/0.1.0.tar.gz',
+    keywords=['ETL', 'blob', 'dependency', 'graph', 'svg', 'networkx', 'transform'],
+    install_requires=[
+        'graphviz',
+        'ipython',
+        'networkx',
+        'numpy',
+        'pandas',
+        'pydot',
+    ],
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: ETL Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.7',
+    ],
+)
