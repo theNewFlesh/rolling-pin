39f1850c2ecbac3150dcedca1099fa9c7a9b5fa0
Alex Braun <alexander.g.braun@gmail.com>
2022-11-08 04:25:21 +0000

    lint

--- python/rolling_pin/blob_etl.py
+++ python/rolling_pin/blob_etl.py
@@ -27,7 +27,7 @@ class BlobETL():
     '''
     Converts blob data internally into a flat dictionary that is universally
     searchable, editable and convertable back to the data's original structure,
-    new blob structures or dircted graphs.
+    new blob structures or directed graphs.
     '''
     def __init__(self, blob, separator='/'):
         # type: (Any, str) -> None
