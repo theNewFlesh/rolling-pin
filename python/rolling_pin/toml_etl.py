3577a63034972c19b1027638bc55c2e89c7433c5
Alex Braun <alexander.g.braun@gmail.com>
2022-11-11 06:46:18 +0000

    added type annotations

--- python/rolling_pin/toml_etl.py
+++ python/rolling_pin/toml_etl.py
@@ -1,3 +1,5 @@
+from typing import Any, Union
+
 from copy import deepcopy
 from pathlib import Path
 import os
@@ -10,9 +12,11 @@ from rolling_pin.blob_etl import BlobETL
 
 class TomlEtlEncoder(toml.TomlArraySeparatorEncoder):
     def __init__(self, _dict=dict, preserve=False, separator=','):
+        # type: (dict, bool, str) -> None
         super().__init__(_dict, preserve, ',\n   ')
 
     def dump_list(self, v):
+        # type: (list) -> str
         if len(v) == 0:
             return '[]'
         if len(v) == 1:
@@ -24,28 +28,35 @@ class TomlEtlEncoder(toml.TomlArraySeparatorEncoder):
 class TomlEtl:
     @classmethod
     def from_string(cls, text):
+        # type: (TomlEtl, str) -> TomlEtl
         return cls(toml.loads(text))
-        
+
     @classmethod
     def from_file(cls, filepath):
+        # type: (TomlEtl, Union[str, Path]) -> TomlEtl
         return cls(toml.load(filepath))
-    
+
     def to_dict(self):
+        # type: () -> dict
         return deepcopy(self._data)
-    
+
     def to_string(self):
+        # type: () -> str
         return toml.dumps(self._data, encoder=TomlEtlEncoder())
-    
+
     def to_file(self, filepath):
+        # type: (Union[str, Path]) -> None
         filepath = Path(filepath)
         os.makedirs(filepath.parent, exist_ok=True)
         with open(filepath, 'w') as f:
             toml.dump(self._data, f, encoder=TomlEtlEncoder())
-    
+
     def __init__(self, data):
+        # type: (dict[str, Any]) -> None
         self._data = data
-        
+
     def edit(self, key, value):
+        # type: (str, str) -> TomlEtl
         data = BlobETL(self._data, separator='.').to_flat_dict()
         if value == ':DELETE:':
             del data[key]
